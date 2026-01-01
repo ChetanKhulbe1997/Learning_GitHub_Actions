e8ea71affa5a9cf944555793d41491b9b432bdbf
ChetanKhulbe1997 <chetankhulbe2016@gmail.com>
2026-01-01 13:28:18 +0530

    Renamed main.tf to ec2.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "mkr_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "MKR_Instance"
-  }
-}
