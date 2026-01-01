4fe4f37fc5bb145e83cd5510ca09ac1e7fd8de4f
ChetanKhulbe1997 <chetankhulbe2016@gmail.com>
2026-01-01 13:34:55 +0530

    Changed the AMI ID

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mkr_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-02dc4cf76a779998b" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"
 
   tags = {
